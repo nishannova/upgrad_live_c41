6b71c451f7ce3de0753cda9866e7af079368d60b
nishannova <nishan.ali495@gmail.com>
2022-06-25 12:32:26 +0530

    Added summation function

---
+++ main.py
@@ -0,0 +1,6 @@
+from summation import sum_
+
+if __name__ == "__main__":
+    x = int(input("Enter an integer"))
+    y = int(input("Enter an integer"))
+    print(f"SUM of X: {x} and Y: {y} is: {sum_(x,y)}")
---
+++ summation.py
@@ -0,0 +1,9 @@
+
+def sum_(x, y):
+    """
+    This is a sumation function that returns sum of 2 integers
+    params: x: Int
+    params: y: Int
+    return: Int: summation of x & y
+    """
+    return x+y
